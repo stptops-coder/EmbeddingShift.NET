cc91f80d88992a25c2c9aa646d0d53e210ae1f26
Stephan Topsakalian <stp.tops@gmail.com>
2025-09-15 03:12:34 +0200

    feat(core): AdditiveShift + DIM=1536 + Vec helpers

---
+++ src/EmbeddingShift.Abstractions/EmbeddingDimensions.cs
@@ -0,0 +1,7 @@
+namespace EmbeddingShift.Abstractions;
+
+public static class EmbeddingDimensions
+{
+    // OpenAI-typische Embedding-Länge
+    public const int DIM = 1536;
+}
---
+++ src/EmbeddingShift.Abstractions/IShift.cs
@@ -0,0 +1,10 @@
+namespace EmbeddingShift.Abstractions;
+
+public interface IShift
+{
+    /// <summary>
+    /// Wendet den Shift auf einen Eingabe-Embedding-Vektor an.
+    /// Erwartet Länge = EmbeddingDimensions.DIM.
+    /// </summary>
+    float[] Apply(ReadOnlySpan<float> input);
+}
