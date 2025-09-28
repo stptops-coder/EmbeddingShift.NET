c6658886452a4ab0ec8ed0ec1d34275508a0a042
Stephan Topsakalian <stp.tops@gmail.com>
2025-09-28 03:52:51 +0200

    Chore: add EvaluatorCatalog and wire ConsoleEval Workflow to catalog (no persistence changes).

--- src/EmbeddingShift.ConsoleEval/Workflow.cs
+++ src/EmbeddingShift.ConsoleEval/Workflow.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using EmbeddingShift.Core.Evaluators;
+
 namespace EmbeddingShift.ConsoleEval
 {
     internal static class Workflow
@@ -7,6 +10,12 @@ namespace EmbeddingShift.ConsoleEval
             => await Task.CompletedTask;
 
         public static async Task EvalAsync(string dataset)
-            => await Task.CompletedTask;
+        {
+            var evaluators = EvaluatorCatalog.Defaults;
+            Console.WriteLine($"[Eval] Dataset='{dataset}', Evaluators={evaluators.Count}");
+            foreach (var ev in evaluators)
+                Console.WriteLine($"  - {ev.GetType().Name} ready");
+            await Task.CompletedTask;
+        }
     }
 }
---
+++ src/EmbeddingShift.Core/Evaluators/EvaluatorCatalog.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using EmbeddingShift.Abstractions;
+
+namespace EmbeddingShift.Core.Evaluators
+{
+    public static class EvaluatorCatalog
+    {
+        public static IReadOnlyList<IShiftEvaluator> Defaults { get; } = new IShiftEvaluator[]
+        {
+            new CosineMeanEvaluator(),
+            new MarginEvaluator(),
+        };
+    }
+}
