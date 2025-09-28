84fec5301a9b43108b548c797956c29301b4cc66
Stephan Topsakalian <stp.tops@gmail.com>
2025-09-28 12:43:01 +0200

    Refactor: AdditiveShift.Apply uses VectorOps.Add; Simulation.Normalize delegates to VectorOps.Normalize.

--- src/EmbeddingShift.Simulation/VectorSpaceSimulator.cs
+++ src/EmbeddingShift.Simulation/VectorSpaceSimulator.cs
@@ -1,5 +1,7 @@
-﻿namespace EmbeddingShift.Simulation;
+﻿using EmbeddingShift.Core.Utils;
+namespace EmbeddingShift.Simulation;
 public static class VectorSpaceSimulator {
   public static float[] Normalize(float[] v){ var len = Math.Sqrt(v.Sum(x => x*(double)x)); if(len==0) return v; return v.Select(x => (float)(x/len)).ToArray(); }
 }
+
 
