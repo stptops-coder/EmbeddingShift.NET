697a969caf6e7b40812e4684c304c2db84adbd1e
Stephan Topsakalian <stp.tops@gmail.com>
2026-01-02 05:34:08 +0100

    Make smoke-all use best non-cancelled posneg result

--- src/EmbeddingShift.ConsoleEval/Commands/SmokeCliCommands.cs
+++ src/EmbeddingShift.ConsoleEval/Commands/SmokeCliCommands.cs
@@ -113,7 +113,8 @@ public static class SmokeCliCommands
             Console.WriteLine("[SMOKE-ALL] Step 4/4: domain mini-insurance posneg-run");
             var rcRun = await DomainCliCommands.ExecuteDomainPackAsync(
                 "mini-insurance",
-                new[] { "posneg-run", "--latest" });
+                new[] { "posneg-run" });
+
 
             if (rcRun != 0)
             {
